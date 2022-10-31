d427380a5f7d72f1e7f2e833628fed5d36d10d88
Alex Wu <alwu@ucsd.edu>
2022-10-30 23:06:38 -0700

    Upload README and Explore

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Lab 5 - Starter
+Name: Alex Wu
+link to  github pages site expose.html page (as well as the link to explore.html):
+https://alex10wu.github.io/Lab5_Starter/expose.html
+https://alex10wu.github.io/Lab5_Starter/explore.html
+
+link to Continuous Integration introduction-to-github: 
+https://github.com/alex10wu/introduction-to-github
--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,56 @@
 // explore.js
-
 window.addEventListener('DOMContentLoaded', init);
 
+const synth = window.speechSynthesis;
+
+const voiceSelect = document.getElementById('voice-select');
+const textToSpeak = document.getElementById('text-to-speak');
+const button = document.querySelector('button');
+const smileImg = document.querySelector("img");
+
+let voices = [];
+
+
 function init() {
-  // TODO
+  // All voices loaded to “Select Voice” dropdown
+  populateVoiceList();
+
+  //The text that was typed should be spoken
+  button.addEventListener("click", (Butt) => {
+    const utterThis = new SpeechSynthesisUtterance(textToSpeak.value);
+    const selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");  
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    synth.speak(utterThis);
+
+    // Open mouth while the synthesizer is speaking
+    smileImg.src = "assets/images/smiling-open.png";
+    utterThis.onend = function () {
+      smileImg.src = "assets/images/smiling.png";
+    };
+  });
+
+}
+
+//Helper function
+//https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis
+function populateVoiceList() {
+  setTimeout(() => {
+    voices = synth.getVoices();
+    for (let i = 0; i < voices.length; i++) {
+      const option = document.createElement("option");
+      option.textContent = `${voices[i].name} (${voices[i].lang})`;
+
+      if (voices[i].default) {
+        option.textContent += " — DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }, 1000);
 }
